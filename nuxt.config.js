9a33cd67c7e38b73354425d19658a13b993a0625
leopoldsza <l.szabat@prismamedia.com>
2017-11-04 18:01:29 +0100

    Init Nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'MYoutube',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  modules: [
+    '@nuxtjs/pwa'
+  ],
+  manifest: {
+    name: 'MYoutube',
+    lang: 'en',
+    theme_color: '#3B8070'
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
